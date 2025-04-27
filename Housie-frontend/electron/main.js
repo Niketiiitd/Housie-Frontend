7d97a2189c457fb981474f7331a2d32a4de78ded
Niteen Kumar <niteen22336@iiitd.ac.in>
2025-04-27 21:08:25 +0530

    electron setup

---
+++ Housie-frontend/electron/main.js
@@ -0,0 +1,24 @@
+// electron/main.js
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true
+    }
+  });
+
+  if (process.env.DEV) {
+    win.loadURL('http://localhost:3000');         // Vite dev server
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => app.quit());
